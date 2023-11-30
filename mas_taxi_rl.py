2c0863894c96208ec9ea16ef45189e3288da1b4f
miguel garcia <miguel.garciabohigues@hotmail.com>
2023-11-30 11:08:19 +0100

    new MAS caller that catch exceptions; added learned Qtables

--- mas_taxi_rl.py
+++ mas_taxi_rl.py
@@ -10,7 +10,7 @@ def merge_agents_q(t1, t2):
     Q = {}
     common_keys = set(t1.keys()).intersection(set(t2.keys()))
     for k in common_keys:
-        Q[k] = (t1[k] + t2[k])/2
+        Q[k] = t1[k] + t2[k]
     
     keys_only_in_1 = set(t1.keys()) - set(t2.keys())
 
@@ -23,11 +23,7 @@ def merge_agents_q(t1, t2):
     return Q
 
 
-async def main():
-    Q = load_object('.','Q10k_MAS_SimpleState') if os.path.exists('./Q10k_MAS_SimpleState.pickle') else {}
-    env_agent = TaxiEnvAgent('taxi_env_agent@gtirouter.dsic.upv.es', 'pass')
-    taxi_agent_1 = TaxiAgent('taxi_agent_1@gtirouter.dsic.upv.es', 'pass', 'taxi_env_agent@gtirouter.dsic.upv.es', Q, inference=False)
-    taxi_agent_2 = TaxiAgent('taxi_agent_2@gtirouter.dsic.upv.es', 'pass', 'taxi_env_agent@gtirouter.dsic.upv.es', Q, inference=False)
+async def main(env_agent, taxi_agent_1, taxi_agent_2):
     await env_agent.start()
     await asyncio.sleep(0.5)
     await taxi_agent_1.start()
@@ -37,14 +33,27 @@ async def main():
         await asyncio.sleep(0.1)      
 
 
-    print('agent_total_reward: ', taxi_agent_1.total_reward)
-    print('agent_total_reward: ', taxi_agent_2.total_reward)
-    print('jobs completed: ', taxi_agent_1.jobs_completed)
-    print('jobs completed: ', taxi_agent_2.jobs_completed)
-    Q = merge_agents_q(taxi_agent_1.q_table, taxi_agent_2.q_table)
-    save_object('.', 'Q10k_MAS_SimpleState', Q)
-    exit()
-
-
 if __name__ == '__main__':
-    spade.run(main())
+    try:
+        Q = load_object('.','Q10k_MAS_CompleteState') if os.path.exists('./Q10k_MAS_CompleteState.pickle') else {}
+        env_agent = TaxiEnvAgent('taxi_env_agent@gtirouter.dsic.upv.es', 'pass')
+        taxi_agent_1 = TaxiAgent('taxi_agent_1@gtirouter.dsic.upv.es', 'pass', 'taxi_env_agent@gtirouter.dsic.upv.es', Q, inference=False)
+        taxi_agent_2 = TaxiAgent('taxi_agent_2@gtirouter.dsic.upv.es', 'pass', 'taxi_env_agent@gtirouter.dsic.upv.es', Q, inference=False)
+              
+        spade.run(main(env_agent, taxi_agent_1, taxi_agent_2))
+
+        print('agent_total_reward: ', taxi_agent_1.total_reward)
+        print('agent_total_reward: ', taxi_agent_2.total_reward)
+        print('jobs completed: ', taxi_agent_1.jobs_completed)
+        print('jobs completed: ', taxi_agent_2.jobs_completed)
+    except:
+        Q = taxi_agent_1.q_table if taxi_agent_1.jobs_completed > taxi_agent_2.jobs_completed else taxi_agent_2.q_table
+        save_object('.', 'Q10k_MAS_CompleteState', Q)
+        print('agent_total_reward: ', taxi_agent_1.total_reward)
+        print('agent_total_reward: ', taxi_agent_2.total_reward)
+        print('jobs completed: ', taxi_agent_1.jobs_completed)
+        print('jobs completed: ', taxi_agent_2.jobs_completed)
+    Q = taxi_agent_1.q_table if taxi_agent_1.jobs_completed > taxi_agent_2.jobs_completed else taxi_agent_2.q_table
+    
+    save_object('.', 'Q10k_MAS_CompleteState', Q)
+    exit()
